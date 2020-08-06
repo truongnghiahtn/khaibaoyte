ffd47760780af6df629deafc038fa579c8dded05
truongnghiahtn <truongnghiahtn@gmail.com>
2020-08-06 14:58:06 +0700

    ok

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class home extends Component {
     render() {
         return (
-            <div> home </div>
+            <div style={{ height: "400px" }}> home </div>
         );
     }
 }
